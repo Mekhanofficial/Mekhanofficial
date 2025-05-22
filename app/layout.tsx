6f99ffb646e292131a99caf4c1e1b71655623f7a
mekhanofficial <prideofketa@gmail.com>
2025-05-22 15:34:03 +0100

    cod

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Providers from "@/providers";
 export const metadata: Metadata = {
   title: "Mekhanofficial",
   description:
-    "Edufacilis is committed to revolutionizing the educational landscape by harnessing the transformative capabilities of Artificial Intelligence (AI). Our mission is to bring cutting-edge technology to the EdTech sector, empowering students with personalized learning experiences while enhancing teachers' productivity.",
+    "My Portfolio.",
 };
 
 export default function RootLayout({
